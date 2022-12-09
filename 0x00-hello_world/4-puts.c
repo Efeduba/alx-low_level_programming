1b81b2b89f27cbd5f07a094a4e5ab1345b6f4568
Efeduba <akudihorkingsley@gmail.com>
2022-12-08 18:19:34 -0800

    4-pudh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@ include <stdio.h>
  * main - print a line of code using puts
  *
  * Return: 0
- */
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multiligual puzzle");
-	return(0);
+	put("\"Programming is like building a mulitiligual puzzle");
+	return (0);
 }
+
